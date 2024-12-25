336d988f6208131b91fa276b6d0d6eddc4344a3e
yrchoi <daylikezero@gmail.com>
2024-12-25 11:39:28 +0900

    #24 refactor: 메인페이지 소스 병합 및 css 수정

--- js/category.js
+++ js/category.js
@@ -2,22 +2,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore, collection, getDocs, doc, query, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA8Do_4_ZDADE64D6v1gbF36_NfaRDvh24",
+    authDomain: "ctrls-miniproject.firebaseapp.com",
+    projectId: "ctrls-miniproject",
+    storageBucket: "ctrls-miniproject.firebasestorage.app",
+    messagingSenderId: "496866464655",
+    appId: "1:496866464655:web:8d866192211cf7699d31fa",
+    measurementId: "G-SGY1JM32JF"
+};
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 
 let categoryArr = [];
-export async function categoryFn() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyA8Do_4_ZDADE64D6v1gbF36_NfaRDvh24",
-        authDomain: "ctrls-miniproject.firebaseapp.com",
-        projectId: "ctrls-miniproject",
-        storageBucket: "ctrls-miniproject.firebasestorage.app",
-        messagingSenderId: "496866464655",
-        appId: "1:496866464655:web:8d866192211cf7699d31fa",
-        measurementId: "G-SGY1JM32JF"
-    };
-
-    // Firebase 인스턴스 초기화
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
+export async function getCategorys() {
     try {
         let docs = await getDocs(collection(db, "category"));
         docs.forEach((doc) => {
@@ -25,7 +26,7 @@ export async function categoryFn() {
         });
         return categoryArr;
     } catch (err) {
-        console.log("categoryFn 에러 발생", err);
+        console.log("getCategorys 에러 발생", err);
         return;
     }
 
@@ -33,14 +34,14 @@ export async function categoryFn() {
 export default categoryArr;
 
 
-export async function getCategory(large) {
+export async function selectCategory(name) {
     try {
         const groupCollection = collection(db, "group");
-        const q = query(groupCollection, where("category","==", large));
+        const q = query(groupCollection, where("category","==", name));
         const querySnapShot = await getDocs(q);
         // 검색 결과가 있는 경우 해당 그룹카드를 생성하여 화면에 출력
     } catch (err) {
-        console.log("getCategory 에러 발생", err);
+        console.log("selectCategory 에러 발생", err);
         return;
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,16 +1,66 @@
+import { getCategorys } from "./category.js";
+import { getGroups, insertGroup } from "./group.js"
 
-import { categoryFn } from "./category.js";
+// 전역변수 index
+let index = 1;
 
-categoryFn().then((categoryArr)=> {
+/* 카테고리 목록 불러오기 */
+getCategorys().then((categoryArr) => {
     categoryArr.forEach(category => {
-       let temp = `
-       <option value="${category.large}">${category.large}</option>
+        let temp = `
+       <option value="${category.name}">${category.name}</option>
        `;
-       $('#categoryBox').append(temp);
+        $('#category').append(temp);
+        $('#categorySearch').append(temp);
     });
 });
 
+/* 카테고리 검색 */
 $("#categoryBtn").click(async function () {
-    const category = $("#categoryBox option:selected").val();
+    const category = $("#categorySearch option:selected").val();
     console.log(category);
 });
+
+/* 소모임 카드 불러오기 */
+getGroups().then((groupArr) => {
+    groupArr.forEach(group => {
+        let dummy = 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'; 
+        let temp_html = `
+        <div class="col mb-5" id="card${group.index}">
+            <div class="card h-100">
+                <!-- Sale badge-->
+                <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">${group.category}</div>
+                <!-- Product image-->
+                <img class="card-img-top" src="${group.image}" alt="..." />
+                <!-- Product details-->
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <!-- Product name-->
+                        <h5 class="fw-bolder">${group.title}</h5>
+                        <!-- Product price-->
+                        ${group.contents}
+                    </div>
+                </div>
+                <!-- Product actions-->
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">상세페이지</a></div>
+                </div>
+            </div>
+        </div>
+        `;
+
+        $('#card-container').append(temp_html);
+        index++;
+    });
+});
+
+/* 소모임 등록하기 */
+$("#postingbtn").click(async function () {
+    let title = $('#title').val();
+    let contents = $('#contents').val();
+    let image = $('#image').val();
+    let category = $('#category').val();
+
+    insertGroup(title, contents, image, category);
+});
+
